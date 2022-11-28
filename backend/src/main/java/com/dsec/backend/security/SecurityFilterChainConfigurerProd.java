add8f84fb6ad9b3fb97d899b9cd217c4ca953d38
Jan Roček <jan.rocek@fer.hr>
2022-11-28 18:56:44 +0100

    Configure for staging

--- backend/src/main/java/com/dsec/backend/security/SecurityFilterChainConfigurerProd.java
+++ backend/src/main/java/com/dsec/backend/security/SecurityFilterChainConfigurerProd.java
@@ -1,8 +1,9 @@
 package com.dsec.backend.security;
 
-import java.util.Arrays;
 import java.util.Optional;
+
 import javax.servlet.http.Cookie;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,10 @@ import org.springframework.security.oauth2.server.resource.web.BearerTokenResolv
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.dsec.backend.security.oauth.CustomOAuth2UserService;
+import com.dsec.backend.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.dsec.backend.security.oauth.OAuth2AuthenticationSuccessHandler;
 import com.dsec.backend.util.cookie.CookieUtil;
 
 @Configuration
@@ -26,25 +31,41 @@ public class SecurityFilterChainConfigurerProd {
 
     private final UserDetailsService myUserDetailsService;
     private final CookieUtil cookieUtil;
+    private final CustomOAuth2UserService userService;
+    private final OAuth2AuthenticationSuccessHandler successHandler;
+    private final HttpCookieOAuth2AuthorizationRequestRepository authorizationRequestRepository;
 
     @Autowired
     public SecurityFilterChainConfigurerProd(@Value("${jwt.cookie.name}") String cookieName,
-            UserDetailsService myUserDetailsService, CookieUtil cookieUtil) {
+            UserDetailsService myUserDetailsService, CookieUtil cookieUtil, CustomOAuth2UserService userService,
+            OAuth2AuthenticationSuccessHandler successHandler,
+            HttpCookieOAuth2AuthorizationRequestRepository authorizationRequestRepository) {
         this.cookieName = cookieName;
         this.myUserDetailsService = myUserDetailsService;
         this.cookieUtil = cookieUtil;
+        this.userService = userService;
+        this.successHandler = successHandler;
+        this.authorizationRequestRepository = authorizationRequestRepository;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // @formatter:off
+
         return http.cors().and().csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(
-                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                .headers().frameOptions().disable().and()
-
-                .oauth2ResourceServer((customizer) -> customizer.jwt().and()
+                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .headers()
+                    .frameOptions().disable()
+                    .and()
+                .formLogin()
+                    .disable()
+                .httpBasic()
+                    .disable()
+                .oauth2ResourceServer(
+                        (customizer) -> customizer.jwt().and()
                         .bearerTokenResolver(getTokenResolver())
                         .authenticationEntryPoint(getAuthenticationEntryPoint()))
 
@@ -53,17 +74,26 @@ public class SecurityFilterChainConfigurerProd {
                         .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
 
                 .authorizeRequests((authorizeRequests) -> authorizeRequests
-                        .antMatchers("/auth/**", "/v3/api-docs*/**", "/")
+                        .antMatchers("/auth/**", "/v3/api-docs*/**", "/", "/github/webhook")
                         .permitAll()
                         .antMatchers("/swagger-ui*/**").denyAll()
                         .antMatchers("/**").access("hasAnyAuthority('SCOPE_USER','SCOPE_ADMIN')")
                         .anyRequest().authenticated())
 
-                .formLogin().disable()
-
+                .oauth2Login()
+                    .authorizationEndpoint()
+                        .authorizationRequestRepository(authorizationRequestRepository)
+                        .and()
+                    .userInfoEndpoint()
+                        .userService(userService)
+                        .and()
+                    .successHandler(successHandler)
+                .and()
                 .userDetailsService(myUserDetailsService)
 
                 .build();
+        
+        // @formatter:on
 
     }
 
@@ -73,12 +103,7 @@ public class SecurityFilterChainConfigurerProd {
 
     private BearerTokenResolver getTokenResolver() {
         return (request) -> {
-            Cookie[] cookies = request.getCookies();
-            Optional<Cookie> cookie = Optional.empty();
-            if (cookies != null) {
-                cookie = Arrays.stream(cookies).filter(c -> c.getName().equals(cookieName))
-                        .findAny();
-            }
+            Optional<Cookie> cookie = cookieUtil.getCookie(request, cookieName);
 
             if (cookie.isPresent()) {
                 return cookie.get().getValue();
