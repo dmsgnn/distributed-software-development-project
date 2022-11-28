5289bad53bd0b6d85b29280be901918af652bb03
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2022-11-28 17:31:47 +0100

    Pulled repository and output now are named using timestamp, and they are deleted when analysis is completed

--- tool-script/script-gitleaks/main.py
+++ tool-script/script-gitleaks/main.py
@@ -2,6 +2,7 @@ import json
 import subprocess
 from flask import Flask
 from flask import request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,12 +14,22 @@ def run_gitleaks():
     param = request.json
     link = param['link']
 
-    # A new shell is opened in order to clone the repository
-    subprocess.run(['git clone ' + link, '-l'], shell=True)
+    # Name of pulled repository
+    repo_name = link.split("/")[len(link.split("/")) - 1].split(".")[0]
+
+    # Timestamp
+    ts = str(datetime.now()).split()[1]
+
+    # Directory and file name
+    directory_name = ts
+    file_name = 'output' + ts + '.json'
+
+    # A new shell is opened in order to clone the repository, saving it in a directory called timestamp
+    subprocess.run(['git clone ' + link + ' ' + directory_name, '-l'], shell=True)
 
     # A new shell is opened in order to run the tool to analyse all the pulled repository
     output = subprocess.run(
-        ['gitleaks detect --no-git -r=output.json -s=' + link.split("/")[len(link.split("/")) - 1].split(".")[0], '-l'],
+        ['gitleaks detect --no-git -r=' + file_name + ' -s=' + directory_name, '-l'],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
     # The output of the tool is saved
@@ -26,8 +37,12 @@ def run_gitleaks():
     result += output.stdout.decode('utf-8')
 
     # The analysis result is taken from the json file
-    out = open("output.json")
+    out = open(file_name)
     parsed = json.load(out)
+
+    # Deletion of output file and pulled repository
+    subprocess.run(['rm ' + file_name], shell=True)
+    subprocess.run(['rm -r ' + directory_name], shell=True)
 
     # The json result is returned 
     return parsed
