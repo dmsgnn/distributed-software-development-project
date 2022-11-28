1d1f4f20ef50e5690346d1cccb2ac23b9e2ffe9d
Jan Roček <jan.rocek@fer.hr>
2022-11-29 00:15:00 +0100

    Set cookie sameSite to None

--- backend/src/main/java/com/dsec/backend/util/cookie/CookieUtilProd.java
+++ backend/src/main/java/com/dsec/backend/util/cookie/CookieUtilProd.java
@@ -64,7 +64,7 @@ public class CookieUtilProd implements CookieUtil {
         response.addHeader(HttpHeaders.SET_COOKIE,
                 ResponseCookie.from(name, value)
                         .httpOnly(true).path("/api")
-                        .maxAge(maxAge).secure(true).sameSite("Lax").build().toString());
+                        .maxAge(maxAge).secure(true).sameSite("None").build().toString());
     }
 
     @Override
@@ -75,7 +75,7 @@ public class CookieUtilProd implements CookieUtil {
                 if (cookie.getName().equals(name)) {
                     response.addHeader(HttpHeaders.SET_COOKIE,
                             ResponseCookie.from(name, "").httpOnly(true).path("/api")
-                                    .maxAge(0).secure(true).sameSite("Lax").build().toString());
+                                    .maxAge(0).secure(true).sameSite("None").build().toString());
                 }
             }
         }
