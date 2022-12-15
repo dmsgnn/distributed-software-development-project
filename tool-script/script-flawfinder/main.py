a33487d6156001b4ac68d9fd39ee33c1f2224236
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2022-12-15 14:34:35 +0100

    Exported decryption function in util.py file

--- tool-script/script-flawfinder/main.py
+++ tool-script/script-flawfinder/main.py
@@ -6,44 +6,9 @@ from flask import Flask
 from flask import request
 from datetime import datetime
 
-import base64
-import hashlib
-from Cryptodome.Cipher import AES
-
-HASH_NAME = "SHA256"
-IV_LENGTH = 12
-ITERATION_COUNT = 65536
-KEY_LENGTH = 32
-SALT_LENGTH = 100
-TAG_LENGTH = 16
+import util
 
 app = Flask(__name__)
-
-
-# Algorithm used -> AES/GCM/NoPadding, PBKDF2WithHmacSHA256
-# Function used to obtain the secret key according to the algorithm used
-def get_secret_key(password, salt):
-    return hashlib.pbkdf2_hmac(HASH_NAME, password.encode(), salt, ITERATION_COUNT, KEY_LENGTH)
-
-
-# Function used to decrypt the received token
-def decrypt(password, cipher_message):
-    # Decode of cipher text
-    decoded_cipher_byte = base64.b64decode(cipher_message)
-
-    # Splitting the decoded cipher in iv, salt, message and tag
-    iv = decoded_cipher_byte[:IV_LENGTH]
-    salt = decoded_cipher_byte[IV_LENGTH:(IV_LENGTH + SALT_LENGTH)]
-    encrypted_message_byte = decoded_cipher_byte[(IV_LENGTH + SALT_LENGTH):-TAG_LENGTH]
-    tag = decoded_cipher_byte[-TAG_LENGTH:]
-
-    # Computation of secret key using password and salt
-    secret = get_secret_key(password, salt)
-    cipher = AES.new(secret, AES.MODE_GCM, iv)
-
-    # Decrypt and return message
-    decrypted_message_byte = cipher.decrypt(encrypted_message_byte)
-    return decrypted_message_byte.decode("utf-8")
 
 
 # POST request
@@ -57,7 +22,7 @@ def run_gitleaks():
 
     # Password used to generate the secret key and decrypt the token
     password = os.environ['PASS']    # password is retrieved from environment variables
-    decrypted_token = decrypt(password, token)
+    decrypted_token = util.decrypt(password, token)
 
     link = "https://" + decrypted_token + "@github.com/" + username + "/" + repository + ".git"
 
